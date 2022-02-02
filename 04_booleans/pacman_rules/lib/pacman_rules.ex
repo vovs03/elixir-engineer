d88d4220942ae733355f4ff2226194c18f4a35dc
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2022-02-03 02:50:56 +0300

    Add: Packman Rules | test12 failed

---
+++ 04_booleans/pacman_rules/lib/pacman_rules.ex
@@ -0,0 +1,25 @@
+defmodule Rules do
+  def eat_ghost?(power_pellet_active, touching_ghost) do
+    # Please implement the eat_ghost?/2 function
+    #power_pellet_active = true
+    #touching_ghost = true
+    power_pellet_active and touching_ghost
+  end
+
+  def score?(touching_power_pellet, touching_dot) do
+    # Please implement the score?/2 function
+    touching_power_pellet or touching_dot
+  end
+
+  def lose?(power_pellet_active, touching_ghost) do
+    # Please implement the lose?/2 function
+    not power_pellet_active
+  end
+
+  def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
+    # Please implement the win?/3 function
+    IO.inspect(has_eaten_all_dots)
+    #IO.inspect(power_pellet_active)
+    #IO.inspect(touching_ghost)
+  end
+end
