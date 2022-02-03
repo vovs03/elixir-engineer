2cad794a779be7ec433b5ce4483de21634156b52
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2022-02-04 01:57:23 +0300

    Refacoring: Mayby clear Code for 3 func | win - hard now

--- 04_booleans/pacman_rules/lib/pacman_rules.ex
+++ 04_booleans/pacman_rules/lib/pacman_rules.ex
@@ -1,34 +1,22 @@
 defmodule Rules do
+
   def eat_ghost?(power_pellet_active, touching_ghost) do
-    # Please implement the eat_ghost?/2 function
-    #power_pellet_active = true
-    #touching_ghost = true
-    power_pellet_active and touching_ghost
+    # recompile force: true
+    power_pellet_active or touching_ghost |> IO.inspect(label: "Test 1")
+    power_pellet_active and touching_ghost |> IO.inspect(label: "Test 2")
   end
 
   def score?(touching_power_pellet, touching_dot) do
-    # Please implement the score?/2 function
-    touching_power_pellet or touching_dot
+    touching_power_pellet or touching_dot |> IO.inspect(label: "Test 4,5,6")
   end
 
   def lose?(power_pellet_active, touching_ghost) do
-    # Please implement the lose?/2 function
-    not power_pellet_active
-
-    #IO.inspect(power_pellet_active)
-    #IO.inspect(touching_ghost)
+    power_pellet_active or touching_ghost |> IO.inspect(label: "Test 7")
+    !power_pellet_active |> IO.inspect(label: "Test 8,9")
   end
 
   def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
-    # Please implement the win?/3 function
-
-    IO.inspect(has_eaten_all_dots)
-    IO.inspect(power_pellet_active)
-    IO.inspect(touching_ghost)
-
-    has_eaten_all_dots or power_pellet_active
-    (not (power_pellet_active or touching_ghost))
-
-
+    has_eaten_all_dots and touching_ghost |> IO.inspect(label: "Test xx")
+    !power_pellet_active |> IO.inspect(label: "Test Power")
   end
 end
